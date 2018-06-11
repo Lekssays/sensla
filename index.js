8826a489557413b702d1135bd631434971e52a9f
Ahmed Lekssays <lekssaysahmed@gmail.com>
2018-06-11 03:12:31 +0000

    change names to songs

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 // cryptographic settigns
-const Blockchain = require('./Blockchain.js');
-const Block = require('./Block.js');
+const Blockchain = require('./Sensla.js');
+const Block = require('./Msmar.js');
 
 var myChain = new Blockchain();
 
-myChain.addBlock(new Block("06/11/2018", 
+myChain.freeBird(new Block("06/11/2018", 
     { 
         name: "Ahmed Lekssays", 
         username:"lekssays", 
-        from: "Imi-N-Tanoute"
+        from: "Imi-N-Tanoute",
+        song: "Stairway to Heaven"
     }
 ));
 
-myChain.addBlock(new Block("06/12/2018", 
+myChain.freeBird(new Block("06/12/2018", 
     { 
         name: "Mohammed Khalil Ait Brahim", 
         username:"khalilwtf", 
-        from: "Meknes"
+        from: "Meknes",
+        song: "Little Wing"
     }
 ));
 
 // to test it.. I will need to implement the mining function
 console.log(JSON.stringify(myChain, null, 4));
-console.log("Is blockchain valid? " + myChain.checkValid());
+console.log("Is blockchain valid? " + myChain.getLucky());
